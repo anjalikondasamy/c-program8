6a7e6ba76fed67814fc8e620e888295a5b046c62
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-03-21 16:49:13 +0530

    multipleof7.c
    good

---
+++ multipleof7.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("enter the number");
+scanf("%d",&n);
+if((n%7)==0)
+{
+printf("number is multiple of 7");
+}
+else
+{
+printf("number is not a multiple of 7");
+}
+}
